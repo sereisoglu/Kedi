c2cf9534d537277c5210966cff790f5d3657b6e1
Saffet Emin Reisoğlu <29237103+sereisoglu@users.noreply.github.com>
2024-02-01 13:40:16 +0300

    Initial Commit

---
+++ Kedi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kedi
+//
+//  Created by Saffet Emin Reisoğlu on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kedi/KediApp.swift
@@ -0,0 +1,17 @@
+//
+//  KediApp.swift
+//  Kedi
+//
+//  Created by Saffet Emin Reisoğlu on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct KediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
